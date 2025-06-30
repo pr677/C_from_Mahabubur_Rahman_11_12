fc7b3c006feade250e0f991ee2b0b24f343819d7
pr677 <pronotoshk@gmail.com>
2025-06-30 23:05:34 +0600

    Create practice11.c
    This is C programming code for Determining whether a number is even or odd.

---
+++ practice11.c
@@ -0,0 +1,23 @@
+/*
+Determining whether a number is even or odd
+find_even_odd.c
+practice11.c
+*/ 
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    if ((n %2)==0)
+    printf("The number %d is even ", n);
+    else
+    printf("The number %d is odd", n );
+
+    return 0;
+}
+
+
